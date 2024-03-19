58e16d872fd6f30f1f5e5cf815a1c1c0f00e40ad
Philippe Tessier <philippe.tessier@getmaintainx.com>
2024-03-19 13:52:31 -0400

    Patch typings imported from dom library

--- packages/openapi-fetch/src/index.d.ts
+++ packages/openapi-fetch/src/index.d.ts
@@ -27,6 +27,17 @@ export interface ClientOptions extends Omit<RequestInit, "headers"> {
   headers?: HeadersOptions;
 }
 
+interface Headers {
+  append(name: string, value: string): void;
+  delete(name: string): void;
+  get(name: string): string | null;
+  has(name: string): boolean;
+  set(name: string, value: string): void;
+  forEach(callbackfn: (value: string, key: string, parent: Headers) => void, thisArg?: any): void;
+}
+
+type HeadersInit = Array<[string, string]> | Record<string, string> | Headers;
+
 export type HeadersOptions =
   | HeadersInit
   | Record<
